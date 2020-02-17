eddc1a66ffc141abbd8497efb9e719a748b77ee2
MauricioArgu <mhernandez@4apps.com.co>
2020-02-17 15:41:48 -0600

    Initial commit

---
+++ app/src/main/java/com/example/testedgepush/MyFirebaseInstanceIdService.java
@@ -0,0 +1,17 @@
+package com.example.testedgepush;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class MyFirebaseInstanceIdService extends FirebaseInstanceIdService {
+    @Override
+    public void onTokenRefresh() {
+        String DeviceToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d("DEVICE TOKEN: ",DeviceToken);
+    }
+}
